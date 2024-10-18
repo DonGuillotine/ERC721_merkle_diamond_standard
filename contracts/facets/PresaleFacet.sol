c288ef3c5db82b83e54ad0e30252aef37fbd2a8b
DonGuillotine <infect3dlab@gmail.com>
2024-10-18 13:39:26 +0100

    Refactor: Modified Presale facet to use shared storage

--- contracts/facets/PresaleFacet.sol
+++ contracts/facets/PresaleFacet.sol
@@ -1,33 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../libraries/LibDiamond.sol";
 
-contract PresaleFacet is Ownable, ReentrancyGuard {
+contract PresaleFacet {
     uint256 public constant PRICE_PER_TOKEN = 0.033333333333333333 ether;
     uint256 public constant MIN_PURCHASE = 0.01 ether;
 
-    bool public presaleActive = false;
-
     event TokensPurchased(address indexed buyer, uint256 amount);
 
-    function setPresaleActive(bool _active) external onlyOwner {
-        presaleActive = _active;
+    function setPresaleActive(bool _active) external {
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.diamondStorage().presaleActive = _active;
     }
 
-    function buyTokens() external payable nonReentrant {
-        require(presaleActive, "Presale is not active");
+    function buyTokens() external payable {
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        require(ds.presaleActive, "Presale is not active");
         require(msg.value >= MIN_PURCHASE, "Below minimum purchase amount");
 
         uint256 tokenAmount = msg.value / PRICE_PER_TOKEN;
         require(tokenAmount > 0, "Not enough ETH sent");
 
-        
         emit TokensPurchased(msg.sender, tokenAmount);
     }
 
-    function withdrawFunds() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+    function withdrawFunds() external {
+        LibDiamond.enforceIsContractOwner();
+        payable(LibDiamond.contractOwner()).transfer(address(this).balance);
     }
 }
